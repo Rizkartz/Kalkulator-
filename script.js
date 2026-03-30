56e635554727813f2698b390793d29676279b6ca
Rizky <140326072+Rizkartz@users.noreply.github.com>
2026-03-30 22:16:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -29,10 +29,52 @@ function handleInput(value) {
 
         display.value = String(parseFloat(display.value) / 100);
     } 
+ 
+    else if (value === "sin") {
+        display.value = String(Math.sin(parseFloat(display.value)));
+    }
+    else if (value === "cos") {
+        display.value = String(Math.cos(parseFloat(display.value)));
+    }
+    else if (value === "tan") {
+        display.value = String(Math.tan(parseFloat(display.value)));
+    }
+    else if (value === "ln") {
+        display.value = String(Math.log(parseFloat(display.value)));
+    }
+    else if (value === "∛x") { // Akar pangkat 3
+        display.value = String(Math.cbrt(parseFloat(display.value)));
+    }
+    else if (value === "Rad") {
+        // Konversi angka (Derajat) menjadi Radian
+        display.value = String(parseFloat(display.value) * (Math.PI / 180));
+    }
+    else if (value === "x!") {
+        // Logika Faktorial (if 5! = 5x4x3x2x1)
+        let n = parseInt(display.value);
+        if (n < 0) {
+            display.value = "Error"; // Faktorial ga bisa minus
+        } else {
+            let result = 1;
+            for (let i = 2; i <= n; i++) {
+                result *= i;
+            }
+            display.value = String(result);
+        }
+    }
+    else if (value === "π") { // Simbol Phi
+        // Munculin angka 3.14159... bla bla
+        if (display.value === "0") {
+            display.value = Math.PI;
+        } else {
+            display.value += Math.PI;
+        }
+    }
+  
     else {
 
         if (display.value === "0" && value !== ".") {
-            display.value = "";
+            display.value = ""; 
         }
         display.value += value;
     }
@@ -40,18 +82,17 @@ function handleInput(value) {
 
 function calculate() {
     try {
-
         const expression = display.value
             .replace(/×/g, "*")
             .replace(/÷/g, "/");
             
-
         const result = new Function('return ' + expression)();
 
         if (isNaN(result) || !isFinite(result)) {
             display.value = "Error";
         } else {
-            display.value = result;
+
+            display.value = Math.round(result * 100000000) / 100000000; 
         }
 
     } catch {
@@ -59,17 +100,14 @@ function calculate() {
     }
 }
 
-
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         handleInput(button.textContent);
     });
 });
 
-
 document.addEventListener("keydown", function(event) {
     const key = event.key;
-
 
     const keyMap = {
         "Enter": "=",
@@ -86,5 +124,4 @@ document.addEventListener("keydown", function(event) {
     } else if (keyMap[key]) {
         handleInput(keyMap[key]);
     }
-
 });
